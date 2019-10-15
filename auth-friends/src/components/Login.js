215c8cfd3b406ac96b9b42276f7114715eb96e89
Arielle Hollingsworth <arizephyr123@gmail.com>
2019-10-15 14:52:05 -0700

    Login.js completed

--- auth-friends/src/components/Login.js
+++ auth-friends/src/components/Login.js
@@ -1,3 +1,61 @@
-import React from 'react';
+import React from "react";
 
-import { axiosWithAuth } from '../utils/axiosWithAuth'
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+import { Redirect } from "react-router-dom";
+
+class Login extends React.Component {
+  state = {
+    credentials: {
+      username: "",
+      password: ""
+    }
+  };
+  handleChange = e => {
+    this.setState({
+      credentials: {
+        ...this.state.credentials,
+        [e.tarkget.name]: e.target.value
+      }
+    });
+  };
+
+  login = e => {
+    e.preventDefault();
+    //login to retrieve the JWT token
+    //add the token to localStorage
+    //route to '/protected' landing page
+    axiosWithAuth()
+      .post("/api/login", this.state.credentials)
+      .then(res => {
+        localStorage.setItem("token", res.data.payload);
+        this.props.history.push("/protected");
+      })
+      .catch(err => console.log(err.response));
+  };
+  render() {
+    if (localStorage.getItem("token")) {
+      return <Redirect to="protected" />;
+    }
+    return (
+      <div>
+        <form onSubmit={this.login}>
+          <input
+          type='text'
+          name='username'
+          value={this.state.credentials.username}
+          onChange={this.handleChange}
+          />
+          <input
+          type='password'
+          name='password'
+          value={this.state.credentials.password}
+          onChange={this.handleChange}
+          />
+          <button>Log In</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
